3a93fd0b7ab32cef9780f2dd8c9c2fe32d7e4b83
Mahesh Babu <mahesh96.kotha@gmail.com>
2022-12-07 21:53:23 +0530

    card title content added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+// https://www.uidesigndaily.com/posts/figma-pricing-card-subscribe-day-1566
 const Card = () => {
   return (
     <div className="card">
-      <div className="card_wraper"></div>
+      <div className="card_wraper">
+        <h1 className="card_title">
+          try <span>free</span> for 4 weeks
+        </h1>
+        <p>we uncover the factor around the clock, all over the globe</p>
+      </div>
     </div>
   );
 };
