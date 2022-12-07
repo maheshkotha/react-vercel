cb7f0e9c9457ee57015a355bdc1310a4d2e30f83
Mahesh Babu <mahesh96.kotha@gmail.com>
2022-12-07 21:33:30 +0530

    card component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To My World...!</h1>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card_wraper"></div>
+    </div>
+  );
+};
+
+export default Card;
